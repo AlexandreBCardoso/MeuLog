ab04f45cd7f5a2bba2c2a27e1275e6d44dcd6db9
Alexandre Cardoso <developer.ios.alexandre@gmail.com>
2023-07-12 23:46:32 -0300

    update MeuLog

--- Sources/MeuLog/MeuLog.swift
+++ Sources/MeuLog/MeuLog.swift
@@ -1,6 +1,5 @@
 public struct MeuLog {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
+    public static func log(message: String) {
+        print("[LOGGER]: \(message)")
     }
 }
